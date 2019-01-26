5fe0849e4712f7f851f165e53f70277936a556ff
sakshi bharti <sakshibharti1@gmail.com>
2019-01-26 12:08:22 -0500

    restructured program

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next)=> {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', (req, res, next)=> {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/product', (req, res, next)=> {
+  res.render('index', { title: 'Product' });
+});
+
+router.get('/services', (req, res, next)=> {
+  res.render('index', { title: 'Services' });
 });
 
 module.exports = router;
