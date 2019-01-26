7b66e49bcddfcae16b0d6715fc3955c5547ef921
sakshi bharti <sakshibharti1@gmail.com>
2019-01-26 12:00:03 -0500

    restructure project

---
+++ public/Scripts/app.js
@@ -0,0 +1,7 @@
+/*custom javascript goes here*/
+(function(){
+    function Start(){
+        console.log(`%c App Started...`, "font-size: 20px; color: blue; font-weight: bold");
+    }
+window.addEventListener("load",Start);
+})();
